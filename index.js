7e66917058fd13a6e13ccebe4bbff197641a1e7d
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-28 03:15:15 -0700

    added index page, 404 page, and home page

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ require('dotenv').config()
 const express = require('express')
 const app= express()
 
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 //Home page
 app.get('/', (req, res)=> {
-    res.send('Hello world!')
+    console.log('home page')
+    res.render('home')
 })
 //Wildcard route/error page
 app.get('*', (req, res)=> {
-    res.send('error404')
+    console.log('error page')
+    res.render('error404')
 })
 app.listen(process.env.PORT, ()=> {
     console.log('Wakey, Wakey')
