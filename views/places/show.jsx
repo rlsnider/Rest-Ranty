71d0bae186cbb8a98b70ae61fa995c627af0815d
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-29 01:57:18 -0700

    show route with edit/delete buttons done

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,39 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <div>
+                {data.place.pic}
+            </div>
+            <div>
+                <h2>Ratings</h2>
+                <p>No Ratings yet</p>
+            </div>
+            <div>
+                <h2>Description</h2>
+                <p>Located in {data.place.city}, {data.place.state} serving {data.place.cuisines}</p>
+            </div>
+            <div>
+                <h2>Comments</h2>
+                <p>No Comments yet</p>
+            </div>
+            <a href="" className="btn btn-warning"> 
+                Edit
+            </a>  
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+            <button type="submit" className="btn btn-danger">
+                Delete
+            </button>
+        </form>     
+
           </main>
         </Def>
     )
 }
 
 module.exports = show
+
